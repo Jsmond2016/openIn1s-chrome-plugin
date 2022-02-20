4dd1e6a0210d0bc6eebcf9ac0898e056349ef7bb
Jsmond2016 <Jsmond2016@gmail.com>
2022-02-20 16:38:51 +0800

    feat: 更新配置

--- js/index.js
+++ js/index.js
@@ -1,82 +1,38 @@
-const { pathname, hostname } = window.location
-const isGithub1sCode = hostname === "github1s.com" && pathname.lastIndexOf("/") > 0
-const isGithubRepository = hostname === "github.com" && pathname.lastIndexOf("/") > 0
 
 
-jump()
-
-
-// 具体实现
-function jump() {
-
-  if (isGithubRepository || isGithub1sCode) {
-    const btn = createButton()
-    const button = setText(btn)
-    const newBtn = addClick(button)
-    appendToBody(newBtn)
-  }
+const switchDomMap = {
+  gitghub1s: 'switch-github1s',
+  mask: 'switch-mask',
 }
 
 
-function createButton() {
-  const button = document.createElement("button")
-  button.textContent = isGithubRepository ? "跳至github1s" : "跳至github仓库"
-  return button
-}
+registerEventLisntener();
+initConfig()
 
-function setText(node) {
-  if (!node) return node
-  const styleColor = isGithubRepository ? "black" : "white"
-  const style = `width: 111px;
-  height: 28px;
-  vertical-align: top;
-  position: fixed;
-  top: 125px;
-  right: -80px;
-  color: ${styleColor};
-  border: 2px solid gray;
-  border-radius: 6px 6px;
-  background: inherit;
-  z-index: 999;
-  cursor: pointer;
-  transition-property: right;
-  transition-duration: 1s;
-  transition-timing-function: ease;
-  `
-  node.style = style
-  node.onmouseover = function() {
-    node.style.right = `2px`
-  }
-  node.onmouseout = function () {
-    node.style.right = "-84px"
-   }
-  return node
-}
 
-function addClick(node) {
-  if (!(typeof node === "object" && node instanceof HTMLElement)) return node
 
-  const clickFn = () => {
-    let url = ""
-    const _1surl = "https://www.github1s.com"
-    const githubUrl = "https://www.github.com"
+// -------分割线---------
 
-    if (isGithubRepository) {
-      url = `${_1surl}${pathname}`
+function initConfig() {
+  chrome.storage.sync.get(Object.keys(switchDomMap), function(result) {
+    for (let k in result) {
+      const $dom = document.getElementById(switchDomMap[k]);
+      if (!$dom) return
+      $dom.checked = result[k]
     }
-    if (isGithub1sCode) {
-      url = `${githubUrl}${pathname}`
+  });
+}
+
+
+function registerEventLisntener() {
+  for (let key in switchDomMap) {
+    const $dom = document.getElementById(switchDomMap[key]);
+    $dom.onchange = function(event) {
+      chrome.storage.sync.set({ [key]: event.target.checked }, function() {
+        console.log(key + 'changed');
+      })
     }
-
-    const tempWindow = window.open("_blank")
-    tempWindow.location.href = url
-
   }
-  node.onclick = clickFn
-  return node
 }
 
 
-function appendToBody(node) {
-  document.body.appendChild(node)
-}
